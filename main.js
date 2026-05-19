0933789854eb76332a695868f466336a1ae26ab9
zzckyy <zackymm2080@gmail.com>
2026-05-19 20:50:53 +0700

    feat: display image, change display image. js script

---
+++ main.js
@@ -0,0 +1,14 @@
+const img1 = "img/charis.jpg";
+const img2 = "img/cabinet.jpg";
+const img3 = "img/mirror.jpg";
+const img4 = "img/sofa.jpg";
+
+function imageDisplay(selected, imageSrc){
+ document.getElementById("displayimg").src = imageSrc;
+
+ const button = document.querySelectorAll(".pill button");
+
+ button.forEach(btn => {btn.classList.remove("active")});
+
+ selected.classList.add("active");
+}
